a97d79a61de37bf8276964e660d73235286e172f
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-12-01 19:01:38 +0300

    123

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -8,14 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
 
-        fields = (
-            'id',
-            'text',
-            'author',
-            'group',
-            'image',
-            'pub_date',
-        )
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
